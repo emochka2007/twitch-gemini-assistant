c539c97afde41bbe021e1e91d33fb0cebfa72569
emochka2007 <vrotexwork@gmail.com>
2025-07-02 20:28:12 +0200

    feat: poller

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ mod pg;
 use crate::event_poller::EventPoller;
 use crate::pg::pg::PgConnect;
 
-mod donation_alerts;
 mod event_poller;
 mod prompt;
 mod terminal;
@@ -13,7 +12,6 @@ mod twitch;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
     dotenvy::dotenv().expect("Env file is not loaded into the project");
-    // fetch_latest_donations().await?;
     let pool = PgConnect::create_pool_from_env()?;
     let client = pool.get().await?;
     PgConnect::run_migrations(&client).await?;
